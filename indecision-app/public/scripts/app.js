5af7380b6b6c1a96ba9cd5fca9126d850ad8692c
leopoldkwok <leopoldkwok@gmail.com>
2018-03-01 21:28:48 +0000

    Manual Data Binding

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -60,13 +60,18 @@ var template = React.createElement(
 
 var count = 0;
 var addOne = function addOne() {
-  console.log('addOne');
+  count++;
+  renderCounterApp();
 };
 var minusOne = function minusOne() {
-  console.log('minusOne');
+  // subtract 1 from count - rerender
+  count--;
+  renderCounterApp();
 };
 var reset = function reset() {
-  console.log('reset');
+  // set count to 0 a rerender
+  count = 0;
+  renderCounterApp();
 };
 
 var templateTwo = React.createElement(
@@ -98,3 +103,35 @@ var templateTwo = React.createElement(
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(templateTwo, appRoot);
+
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -36,13 +36,18 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-  console.log('addOne');
+  count++;
+  renderCounterApp();
 };
 const minusOne = () => {
-  console.log('minusOne');
+  // subtract 1 from count - rerender
+  count--;
+  renderCounterApp();
 };
 const reset = () => {
-  console.log('reset');
+  // set count to 0 a rerender
+  count = 0;
+  renderCounterApp();
 };
 
 const templateTwo = (
@@ -57,3 +62,18 @@ const templateTwo = (
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(templateTwo, appRoot);
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
