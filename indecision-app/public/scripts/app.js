25e285792b9fa9eaa93de1bae64ca41c6e7990d7
leopoldkwok <leopoldkwok@gmail.com>
2018-03-01 09:53:07 +0000

    Exploring JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,61 @@ console.log('App.js is running!');
 
 // JSX - Javascript XML
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item Two'
+    )
+  )
 );
+
+// Create a templateTwo var JSX expression
+// div
+//  h1 -> Andrew Mead
+// p -> Age: 36
+// p -> Location: Philadelphia
+// Render templateTwo instead of template
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Andrew Mead'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 36'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Philadelphia'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,32 @@
 console.log('App.js is running!');
 
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item One</li>
+      <li>Item Two</li>
+    </ol>
+  </div>
+);
+
+// Create a templateTwo var JSX expression
+// div
+//  h1 -> Andrew Mead
+// p -> Age: 36
+// p -> Location: Philadelphia
+// Render templateTwo instead of template
+
+var templateTwo = (
+  <div>
+    <h1>Andrew Mead</h1>
+    <p>Age: 36</p>
+    <p>Location: Philadelphia</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
