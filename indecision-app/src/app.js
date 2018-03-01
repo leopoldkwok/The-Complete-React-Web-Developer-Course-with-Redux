462264252df4b9436830f5dc25dee98a6417714d
leopoldkwok <leopoldkwok@gmail.com>
2018-03-01 15:01:13 +0000

    ES6 Aside const and let

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -11,14 +11,14 @@ console.log('App.js is running!');
 // only render the subtitle (and p tag) if subtitle exist - logical and operator
 // render new p tag - if options.length > 0 "Here are your options" "No options"
 
-var app = {
+const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
   options: ['One', 'Two']
 };
 
 // JSX - Javascript XML
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
@@ -37,7 +37,7 @@ var template = (
 // p -> Location: Philadelphia
 // Render templateTwo instead of template
 
-var user = {
+const user = {
   name: 'Andrew',
   age: 30,
   location: 'Philadelphia'
@@ -49,7 +49,7 @@ function getLocation(location) {
   }
 }
 
-var templateTwo = (
+const templateTwo = (
   <div>
     <h1>{user.name ? user.name : 'Anonymous'}</h1>
     {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
@@ -57,6 +57,6 @@ var templateTwo = (
   </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
