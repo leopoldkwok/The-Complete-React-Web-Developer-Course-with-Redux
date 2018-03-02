c21db83b0cb152d3f0ade92adb465698e30e3738
leopoldkwok <leopoldkwok@gmail.com>
2018-03-02 01:26:02 +0000

    ES6 Classes Part II

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -15,10 +15,49 @@ class Person {
   }
 }
 
-const me = new Person('Andrew Mead', 26);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
 
-const other = new Person();
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+// Traveller -> Person
+// Add support for homeLocation
+// Override getGreeting
+// 1. Hi. I am Andrew Mead. I'm visiting from Philadelphia
+// 2. Hi. I am Andrew Mead
+
+class Traveller extends Person{
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+    
+    return greeting;
+  }
+};
+
+const me = new Traveller('Andrew Mead', 26, 'Philpadelphia');
+console.log(me.getGreeting());
+
+const other = new Traveller(undefined, undefined, 'Nowhere');
 console.log(other.getGreeting());
-console.log(other.getDescription());
